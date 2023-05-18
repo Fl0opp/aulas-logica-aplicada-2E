480f86c62ad2f866cbf8b1b629d70fe00d065163
rm86524 <rm86524@estudante.fieb.edu.br>
2023-05-18 12:28:03 -0300

    .

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -2,4 +2,9 @@ public class Pessoa {
     // Atributos da pessoa
     public String nome;
     public String sobrenome;
+    // Métodos da classe
+    public void falar (){
+        System.outs.println("falei");
+    }
+
 }
